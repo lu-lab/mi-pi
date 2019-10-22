1d8302948de1de6e32f873ebe7d8453a7b1d4fd2
Bates, Kathleen <kebates@gatech.edu>
2019-10-22 12:08:46 -0400

    bug

--- hardwareSupport/pivy_camera.py
+++ hardwareSupport/pivy_camera.py
@@ -418,7 +418,7 @@ class CameraSupport(multiprocessing.Process):
         cv2.imwrite(imgfile3, im3)
 
         self.width, self.height = self.image_processing_params['image_resolution']
-        mvmnt = delta_movement(im1, im2, self.image_processing_params)
+        mvmnt = delta_movement(im1, im2, 0, self.image_processing_params)
         LED_difference = delta_movement(im2, im3, self.image_processing_params)
         Logger.info('Camera: movement is %s, LED difference is %s' % (mvmnt, LED_difference))
         return mvmnt, LED_difference
